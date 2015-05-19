f0a1e148d8ec059abeb277931be2275095c3c75d
Chris Schenk <chris.schenk@gmail.com>
2015-05-18 22:31:09 -0700

    Added the_post_thumbnail to blog post index

--- content.php
+++ content.php
@@ -13,7 +13,16 @@
 		<?php foundationpress_entry_meta(); ?>
 	</header>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading...', 'foundationpress' ) ); ?>
+		<a href="<?php the_permalink(); ?>">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<div class="row">
+					<div class="column">
+						<?php the_post_thumbnail( '', array('class' => 'th') ); ?>
+					</div>
+				</div>
+			<?php endif; ?>
+		</a>
+		<?php the_content( __( 'Continue reading...', 'FoundationPress' ) ); ?>
 	</div>
 	<footer>
 		<?php $tag = get_the_tags(); if ( ! $tag ) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
